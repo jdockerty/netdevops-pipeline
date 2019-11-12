b7581241f9ff9a5efab4bf9a06c9e434f13e83c8
Jack Dockerty <dockertj@coventry.ac.uk>
2019-11-12 12:55:10 +0000

    VPC Building changed, single gateway.

--- LambdaLocal.py
+++ LambdaLocal.py
@@ -4,12 +4,14 @@ import json
 event = {
   "name": "Jack"
 }
+context = 1
+
 def lambda_handler(event,context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
-context = 1
+        'body': json.dumps('Hello {}, from Lambda!'.format(event['name']))
+    }   
+
 
 print(lambda_handler(event,context))
 
