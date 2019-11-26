27acb30c5c60a2e073988252e53fd7520a977135
Jack Dockerty <jdockerty19@gmail.com>
2019-11-26 19:16:50 +0000

    Semi-automated the config generation for BGP/IPsec on Cisco router, input public IP address and rest is handled, template is copied onto router. YML/JSON files altered to ensure inside tunnel CIDR is fixed to aid consistency from code.

--- BGP_IPsec_config_gen.py
+++ BGP_IPsec_config_gen.py
@@ -10,13 +10,10 @@ def clear():
 
 interface = str(input("Enter the interface connected in GNS3: ")) # {0}
 ip_outside_address_one = str(input("Enter the 1st Public Tunnel Address: ")) # {1}
-#ip_add_two = str(input("Enter the 2nd Public Tunnel Address: "))
-tunnel_one_neighbour = str(input("Enter the tunnel 1 neighbor inside address (first address of CIDR block): ")) # {3}
-#tunnel_two_neighbour = str(input("Enter the tunnel 2 neighbor inside address (first address of CIDR block): ")) #{6}
-my_tunnel_one_CIDR = str(input("Enter the address for the customer tunnel 1 side (2nd address of CIDR block): ")) # {2}
-#my_tunnel_two_CIDR = str(input("Enter the address for the customer tunnel 2 side (2nd address of CIDR block): ")) # {5}
+ip_outside_address_two = str(input("Enter the 2nd Public Tunnel Address: ")) # {2}
+
 clear()
 
 with open("BGP_IPsec_template.txt") as config_file:
     for lines_in_file in config_file:
-        print(lines_in_file.format(interface, ip_outside_address_one, my_tunnel_one_CIDR, tunnel_one_neighbour))
+        print(lines_in_file.format(interface, ip_outside_address_one, ip_outside_address_two))
