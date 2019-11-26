c88d155ff95b496a95443a161d4e4f1c8acc5c82
Jack Dockerty <jdockerty19@gmail.com>
2019-11-26 12:26:33 +0000

    Minor cleanup for config_gen python file.

--- BGP_IPsec_config_gen.py
+++ BGP_IPsec_config_gen.py
@@ -1,5 +1,6 @@
 from os import system, name
-import pyperclip
+
+
 # Run from console, clears the output and can easily Ctrl + A -> Ctrl + C for easy config dump.
 def clear():
 
@@ -10,11 +11,7 @@ def clear():
 interface = str(input("Enter the interface connected in GNS3: "))
 ip_add_one = str(input("Enter the 1st Public Tunnel Address: "))
 ip_add_two = str(input("Enter the 2nd Public Tunnel Address: "))
-
-
-
 clear()
-
 
 with open("BGP_IPsec_template.txt") as config_file:
     for lines_in_file in config_file:
